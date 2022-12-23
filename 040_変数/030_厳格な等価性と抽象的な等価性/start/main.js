5d07b55842c19622bf78aed231ebd01f42e773ed
narazunogin@gmail.com <narazunogin@gmail.com>
2022-12-23 20:02:36 +0900

    20221223

---
+++ 040_変数/030_厳格な等価性と抽象的な等価性/start/main.js
@@ -0,0 +1,20 @@
+function printEquality(a, b) {
+  console.log(a === b);
+  console.log(a == b);
+}
+
+let a = '1';
+let b = 1;
+
+let c = true;
+
+// printEquality(b, c)
+
+let e = "";
+let f = 0;
+
+// printEquality(e, f)
+
+let g = "0";
+
+printEquality(f, g)
---
+++ 040_変数/040_falsyとtruthy/start/main.js
@@ -0,0 +1,8 @@
+let a = parseInt("");
+console.log(Boolean(a));
+
+if(a) {
+  console.log("that's truthy!");
+} else {
+  console.log(a  + " is not truthy value!");
+}

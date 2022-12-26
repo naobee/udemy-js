2d3d7d1440f12960511a73d0358447cc499d0a94
narazunogin@gmail.com <narazunogin@gmail.com>
2022-12-26 19:21:00 +0900

    20221226

---
+++ 040_変数/060_参照とコピー/start/main.js
@@ -0,0 +1,16 @@
+let a = 'hello';
+let b = a;
+b = 'bye';
+console.log(a, b);
+
+let c = {
+  prop: 'hello'
+}
+let d = c;
+console.log(c, d);
+
+d.prop = 'bye';
+console.log(c, d);
+
+d = {};
+console.log(c, d);

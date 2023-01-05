ffbfd637024eebcb444f46cefcd2ccf033206fe1
narazunogin@gmail.com <narazunogin@gmail.com>
2023-01-05 19:49:01 +0900

    20230105

--- 050_関数とオブジェクト１/070_bindとthis/start/main.js
+++ 050_関数とオブジェクト１/070_bindとthis/start/main.js
@@ -8,8 +8,19 @@ const person = {
 }
 person.hello();
 
+const helloTom = person.hello.bind(person);
+
 function fn(ref) {
     ref();
 }
 
-fn(person.hello);
+fn(helloTom);
+
+
+function a (name) {
+  console.log('hello ' + name);
+}
+
+const b = a.bind(null, 'Tim');
+
+b();

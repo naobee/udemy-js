9368ac6a8b3b96353c4cd21ee77e425c33c65d43
narazunogin@gmail.com <narazunogin@gmail.com>
2023-01-10 20:28:49 +0900

    20230110

--- 050_関数とオブジェクト１/910_修了確認_コールバック関数/start/main.js
+++ 050_関数とオブジェクト１/910_修了確認_コールバック関数/start/main.js
@@ -13,6 +13,11 @@ const person = {
 }
 
 // setTimeout(/** ここに追記 */, 1000);
+setTimeout(function() {
+  const hello = person.hello();
+  console.log(hello);
+  }, 1000);
+
 
 /**
  * 問題２：
@@ -24,7 +29,11 @@ const person = {
  * ※alertは第一引数に渡した文字列を画面のダイアログに表
  * 示する関数です。
  */
-
+// alert(setTimeout)
+/*setTimeout(function() {
+  const hello = person.hello();
+  alert(hello);
+  }, 1000);*/
 
 /**
  * 問題３：
@@ -46,7 +55,15 @@ function after1s(callack) {
 }
 
 // この時点で実行します。
-// after1s(obj.greeting);
+after1s(obj.greeting);
+
+let obj1 = {
+  prop: 10
+}
+
+let obj2 = obj1;
+obj1 = {};
+
 
 // この後でgreetingを書き換えます。
 obj.greeting = function() {
@@ -63,32 +80,32 @@ obj.greeting = function() {
  * 
  * ※コールバック関数を用いて実装してください。
  */
-function calcFactory(val) {
+function calcFactory(val, callback) {
     return {
         plus: function(target) {
             const newVal = val + target;
-            console.log(`${val} + ${target} = ${newVal}`);
+            callback(`${val} + ${target} = ${newVal}`);
             val = newVal;
         },
         minus: function(target) {
             const newVal = val - target;
-            console.log(`${val} - ${target} = ${newVal}`);
+            callback(`${val} - ${target} = ${newVal}`);
             val = newVal;
         },
         multiply: function(target) {
             const newVal = val * target;
-            console.log(`${val} x ${target} = ${newVal}`);
+            callback(`${val} x ${target} = ${newVal}`);
             val = newVal;
         },
         divide: function(target) {
             const newVal = val / target;
-            console.log(`${val} / ${target} = ${newVal}`);
+            callback(`${val} / ${target} = ${newVal}`);
             val = newVal;
         }
     };
 }
 
-const calc = calcFactory(10);
+const calc = calcFactory(10, alert);
 calc.plus(5); 
 calc.minus(3); 
 calc.multiply(3);

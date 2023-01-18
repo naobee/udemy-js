8846160df2e3cca7399b1dfe83eab229c33cf9bb
narazunogin@gmail.com <narazunogin@gmail.com>
2023-01-18 18:50:44 +0900

    20230118

--- 060_関数とオブジェクト２/040_instanceof/start/main.js
+++ 060_関数とオブジェクト２/040_instanceof/start/main.js
@@ -1,7 +1,29 @@
 function F(a, b) {
     this.a = a;
     this.b = b;
+    // 以下コメントアウトしているのはオブジェクトリテラル内の処理
+    // const result = new Object();
+    // result.a = 1;
     // return {a: 1};
 }
 
 F.prototype.c = function() {}
+
+const instance = new F(1,2);
+console.log(instance)
+console.log(instance instanceof Object)
+// instanceofは以下の処理を内部で行っている(インスタンスの__proto__とコンストラクターのprototypeの参照先が同じかの確認)
+console.log(instance.__proto__ === F.prototype)
+// 以下はinstanceofの使用例
+// argの中身が配列かそれ以外（例えばオブジェクト）かをチェックし、処理を変える
+function fn(arg) {
+  if(arg instanceof Array) {
+  arg.push('value')
+  } else {
+  arg['key'] = 'value';
+  }
+  console.log(arg)
+ }
+ 
+ fn({});
+ fn([]);

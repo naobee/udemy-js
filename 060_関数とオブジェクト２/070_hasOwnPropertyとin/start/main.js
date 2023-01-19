4acb3bc8f6671d6d88fbc67ce6b8b30f84fee709
narazunogin@gmail.com <narazunogin@gmail.com>
2023-01-19 19:47:43 +0900

    20230119

--- 060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
+++ 060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
@@ -8,3 +8,9 @@ Object.prototype.hello = function() {
 }
 
 const bob = new Person('Bob', 18);
+// hasOwnProperty 指定された引数のプロパティがオブジェクト自身の直下に存在するかを確認
+const result = Person.hasOwnProperty('hello');
+console.log(result);
+// in 指定された名前のプロパティやメソッドが存在するかプロトタイプチェーンを遡って確認
+console.log('name' in bob);
+
--- 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
+++ 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
@@ -6,3 +6,26 @@ function Person(name, age) {
 Person.prototype.hello = function() {
   console.log('hello ' + this.name);
 }
+
+// Personのhelloメソッドを流用して日本人のオブジェクトを整理するJapaneseというコンストラクタをprototype継承によって作成する
+function Japanese(name, age, gender) {
+// Personのコンストラクタ関数の継承、下のthisはJapaneseの関数コンテキストのthisを指す
+  Person.call(this, name, age)
+  this.gender = gender;
+
+}
+
+// Personの prototypeを objectのcreateによって Japaneseの prototypeに格納する
+// Object.create は、第一引数に渡したオブジェクトをプロトタイプに持つ空のオブジェクトを作成する
+Japanese.prototype = Object.create(Person.prototype);
+
+
+Japanese.prototype.bye = function() {
+  console.log('Sayonara ' + this.name);
+}
+
+
+const taro = new Japanese('Taro', '23', 'Male');
+console.log(taro);
+
+taro.bye();

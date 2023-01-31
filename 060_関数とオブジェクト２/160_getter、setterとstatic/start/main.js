78b3ad062a61ec1b5605073b1e033dac67c89cd0
narazunogin@gmail.com <narazunogin@gmail.com>
2023-01-31 19:12:15 +0900

    20230131

--- 060_関数とオブジェクト２/160_getter、setterとstatic/start/main.js
+++ 060_関数とオブジェクト２/160_getter、setterとstatic/start/main.js
@@ -2,3 +2,47 @@ function Person1(name, age) {
 	this.name = name;
 	this.age = age;
 }
+
+Person1.hello = function() {
+  console.log('hello');
+}
+
+Person1.hello();
+
+Object.defineProperty(Person1.prototype, 'name', {
+  get: function() {
+    console.log('hello');
+    return this._name;
+  },
+  set: function(val) {
+    this._name = val;
+  }
+});
+
+const p1 = new Person1('Bob', 23);
+p1.name = 'tom';
+
+console.log(p1.name);
+
+class Person2 {
+  constructor(name, age) {
+	this.name = name;
+	this.age = age;
+}
+
+  get name() {
+    console.log('hello');
+    return this._name;
+  }
+  set name(val) {
+    this._name = val;
+  }
+  
+  static hello() {
+    console.log('hello');
+  }
+}
+
+const p2 = new Person2('Bob', 23);
+
+Person2.hello();
--- 060_関数とオブジェクト２/170_チェーンメソッド/start/main.js
+++ 060_関数とオブジェクト２/170_チェーンメソッド/start/main.js
@@ -6,14 +6,17 @@ class Person {
 
 	hello(person) {
 		console.log(`${this.name} says hello ${person.name}`);
+		return this;
 	}
 
 	introduce() {
 		console.log(`Hi, I'm ${this.name}, ${this.age} years old.`);
+	  return this;
 	}
 
 	shakeHands(person) {
 		console.log(`${this.name} shake hands with ${person.name}.`);
+	  return this;
 	}
 
 	bye(person) {
@@ -24,7 +27,7 @@ class Person {
 const bob = new Person('Bob', 23);
 const tim = new Person('Tim', 33);
 
-bob.hello(tim);
-bob.introduce();
-bob.shakeHands(tim);
-bob.bye(tim);;
+bob.hello(tim)
+  .introduce()
+  .shakeHands(tim)
+  .bye(tim)

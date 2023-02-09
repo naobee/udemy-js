c601827626c3d5a7d86213b8d76a74c2cd7bb9bf
narazunogin@gmail.com <narazunogin@gmail.com>
2023-02-09 18:01:50 +0900

    20230209

--- 070_反復処理/050_forofと反復可能性/start/main.js
+++ 070_反復処理/050_forofと反復可能性/start/main.js
@@ -1,1 +1,22 @@
 const arry = ['a', 'b', 'c'];
+
+arry[4] = 'e';
+
+Object.prototype.methodo = function() {}
+
+Object.defineProperty(arry, 0, {
+  enumerable: false
+})
+
+for(let v of arry) {
+  console.log(v);
+}
+
+for (let key in arry) {
+  // if(obj.hasOwnProperty(key)) {
+  console.log(key, arry[key]);
+  //  }
+}
+
+const d = Object.getOwnPropertyDescriptor(arry, 0);
+console.log(d);

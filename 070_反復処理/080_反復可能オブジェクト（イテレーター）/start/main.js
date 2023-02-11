81a458a26fb50228249ee534f4921c7bb1952738
narazunogin@gmail.com <narazunogin@gmail.com>
2023-02-11 20:06:44 +0900

    20230211

--- 070_反復処理/080_反復可能オブジェクト（イテレーター）/start/main.js
+++ 070_反復処理/080_反復可能オブジェクト（イテレーター）/start/main.js
@@ -1,10 +1,26 @@
-const obj = {
+const items = {
 	prop1: 'value1',
 	prop2: 'value2',
 	prop3: 'value3'
 }
 
-const items = Object.entries(obj);
-for (let item of items) {
-	console.log(item);
+Object.prototype[Symbol.iterator] = function() {
+  const keys = Object.keys(this);
+  let i = 0;
+  let _this = this;
+  return {
+    next() {
+      let key = keys[i++];
+      return {
+        value: [key, _this[key]],
+        done: i > keys.length
+      }
+    }
+  }
+}
+
+// 通常オブジェクトをentriesで配列化することによって、for...ofによる反復処理を可能にする事ができる
+// const items = Object.entries(obj);
+for (let [k, v] of items) {
+	console.log(k, v);
 }

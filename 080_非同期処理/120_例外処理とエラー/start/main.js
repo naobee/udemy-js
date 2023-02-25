0cb011b14f103056235b90a0560e854ecbe5fee1
narazunogin@gmail.com <narazunogin@gmail.com>
2023-02-25 21:11:38 +0900

    20230225

--- 080_非同期処理/120_例外処理とエラー/start/main.js
+++ 080_非同期処理/120_例外処理とエラー/start/main.js
@@ -1,11 +1,50 @@
-
-
-// async function fetchUsers() {
-//   const response = await fetch('users.json');
-//   const json = await response.json();
-//   for(const user of json) {
-//     console.log(`I'm ${user.name}, ${user.age} years old`)
-//   }
+// try {
+//   console.log('start');
+//   throw new Error('error massage');
+//   console.log('end');
+// } catch(e) {
+//   console.error(e)
+// } finally {
+//   console.log('bye');
 // }
 
-// fetchUsers();
+async function fetchUsers() {
+  const response = await fetch('users.json');
+  if(response.ok) {
+    const json = await response.json();
+    if(!json.lenght) {
+      throw new NoDataError('no data found')
+    }
+    return json;
+  }
+}
+
+class NoDataError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'NoDataError';
+  }
+}
+
+
+async function init() {
+  try {
+    const users = await fetchUsers();
+    for(const user of users) {
+      console.log(`I'm ${user.name}, ${user.age} years old`)
+    }
+  } catch(e) {
+    if(e instanceof NoDataError) {
+      console.log(e);
+    } else {
+      console.error('Oops, somethin went wrong');
+    }
+    console.error(e);
+  } finally {
+    console.log('bye')
+  }
+  console.log('end');
+}
+
+
+init();

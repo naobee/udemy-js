83ef74bc94056bc3687de0c950832cdd2f80bbe6
narazunogin@gmail.com <narazunogin@gmail.com>
2023-03-09 20:14:42 +0900

    20230309

--- 100_様々なオブジェクト/010_Proxy/start/main.js
+++ 100_様々なオブジェクト/010_Proxy/start/main.js
@@ -1,1 +1,25 @@
 const targetObj = { a: 0 };
+const handler = {
+  set: function(target, prop, value, receiver) {
+    console.log(`[set]: ${prop}`);
+    // target[prop] = value;
+    throw new Error('cannot add prop.');
+  },
+  get: function(target, prop, receiver) {
+    console.log(receiver);
+    if(target.hasOwnProperty(prop)) {
+      return target[prop];
+    } else {
+      return '-1';
+    }
+    console.log(`[get]: ${prop}`);
+  },
+  deleteProperty: function(target, prop) {
+    console.log(`[delete]: ${prop}`);
+    delete target[prop];
+  },
+}
+const pxy = new Proxy(targetObj, handler);
+// pxy.a = 1;
+console.log(pxy.b);
+// delete pxy.a;

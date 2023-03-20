81c4606d6499505080d0538b5ff42d7999e9eb20
narazunogin@gmail.com <narazunogin@gmail.com>
2023-03-20 20:12:11 +0900

    20230320

--- 100_様々なオブジェクト/910_修了確認_配列/start/main.js
+++ 100_様々なオブジェクト/910_修了確認_配列/start/main.js
@@ -19,6 +19,47 @@ class MyArray extends Array {
 		console.log(`%c ${label}`, 'color: blue; font-weight: 600;', this);
 		return this;
 	}
+	
+	push(val) {
+	  super.push(val);
+	  return this;
+	}
+	
+	forEach(callback) {
+	  for(let i = 0; i < this.length; i++) {
+	    callback(this[i], i, this);
+	  }
+	}
+	
+	map(callback) {
+	  const newInstance = new MyArray();
+	  for(let i = 0; i < this.length; i++) {
+	    const result = callback(this[i], i, this);
+	    newInstance.push(result);
+	  }
+	  return newInstance;
+	}
+	
+	filter(callback) {
+	  const newInstance = new MyArray();
+	  for (let i = 0; i < this.length; i++) {
+	    if(callback(this[i], i, this)) {
+	      newInstance.push(this[i]);  
+	    }
+	  }
+	  return newInstance;
+	}
+	
+	reduce(callback, accu) {
+	  const tmpArry = [...this];
+	  if(accu === undefined) {
+	    accu = tmpArry.shift();
+	  }
+	  for (let i = 0; i < this.length; i++) {
+	    accu = callback(accu, this[i]);
+	  }
+	  return accu;
+	}
 }
 
 function double(v, i, obj) {
@@ -26,16 +67,23 @@ function double(v, i, obj) {
 }
 
 const original = new MyArray(1, 2, 3, 4);
-
+// original.push(6)
 const result = original
-	.map(double)
-	.push(5)
-	.filter(function (v, i) {
+// .forEach(function(v, i, arry){
+//   console.log(v, i, arry);
+// })
+
+ 	.map(double)
+ 	.print('place1')
+ 	.push(5)
+  .filter(function (v, i) {
 		return v > 2;
 	})
+	.print('place1')
+ 	
 	.reduce(function(accu, curr) {
 		return accu + curr;
-	})
+	}, 0)
 
 console.log('%coriginal', 'color: blue; font-weight: bold;', original);
 console.log('%cresult', 'color: red; font-weight: bold;', result);
